9b8ade221c61dfebc1620c0d371c79fdc2b8b9d1
luke-dkk <luke_persson@yahoo.dk>
2026-02-06 16:40:58 +0100

    Changed so as Main now is clean and implemented App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,25 +1,49 @@
 package app;
 
 import app.config.HibernateConfig;
-import app.config.ThymeleafConfig;
-import io.javalin.Javalin;
-import io.javalin.rendering.template.JavalinThymeleaf;
+//import app.config.ThymeleafConfig;
+import app.daos.ChildDAO;
+import app.daos.PlaygroundDAO;
+import app.daos.UserDAO;
+import app.entities.Child;
+import app.entities.Playground;
+import app.entities.User;
+//import io.javalin.Javalin;
+//import io.javalin.rendering.template.JavalinThymeleaf;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PrePersist;
 
 public class App {
-    private static EntityManagerFactory emf;
+    private static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
     public static void initiate() {
-        emf = HibernateConfig.getEntityManagerFactory();
+        PlaygroundDAO playgroundDAO = new PlaygroundDAO(emf);
+        ChildDAO childDAO = new ChildDAO(emf);
+        UserDAO userDAO = new UserDAO(emf);
+
+        User user = User.builder().parentName("Niels").email("niels.dk").password("1234").build();
+        user.setEmail("niels");
+        userDAO.createUser(user);
 
 
-        Javalin app = Javalin.create(config ->
-        {
-            config.staticFiles.add("/public");
-            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-            config.staticFiles.add("/templates");
-        }).start(7072);
+        Playground pg = Playground.builder().name("Fælledparken").capacity(1).latitude(123.3).longitude(3.14).build();
+        playgroundDAO.createPlayground(pg);
 
+        Child child = Child.builder().age(1).gender("fluid").sex("boy").name("Henrik").build();
+        childDAO.createChild(child);
+
+        System.out.println(childDAO.getAllChildren());
+        System.out.println(playgroundDAO.getAllPlaygrounds());
+        System.out.println(userDAO.getAllUsers());
+
+//        Javalin app = Javalin.create(config ->
+//        {
+//            config.staticFiles.add("/public");
+//            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+//            config.staticFiles.add("/templates");
+//        }).start(7072);
+        emf.close();
     }
 }
 
