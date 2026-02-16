0a6b4485f784a8383f3873cc52aa63f5968e76cd
luke-dkk <luke_persson@yahoo.dk>
2026-02-16 12:55:27 +0100

    Changed logs to be set as to better see whats happening.

--- src/main/java/app/config/HibernateBaseProperties.java
+++ src/main/java/app/config/HibernateBaseProperties.java
@@ -11,8 +11,8 @@ final class HibernateBaseProperties {
         props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
         props.put("hibernate.current_session_context_class", "thread");
         props.put("hibernate.show_sql", "true");
-        props.put("hibernate.format_sql", "false");
-        props.put("hibernate.use_sql_comments", "false");
+        props.put("hibernate.format_sql", "true");
+        props.put("hibernate.use_sql_comments", "true");
         props.put("hibernate.hikari.maximumPoolSize", "10");
         props.put("hibernate.hikari.minimumIdle", "2");
         props.put("hibernate.hikari.connectionTimeout", "20000");
