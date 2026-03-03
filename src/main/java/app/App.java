4c95d2a2a31f789ba579be4279a0d62117bf4342
luke-dkk <luke_persson@yahoo.dk>
2026-03-03 17:28:47 +0100

    Initiatlized the populatorm so as to start cleaning up mymain.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,35 +1,78 @@
 package app;
 
 import app.config.HibernateConfig;
-//import app.config.ThymeleafConfig;
-import app.daos.ChildDAO;
+
 import app.daos.PlaygroundDAO;
-import app.daos.UserDAO;
 import app.entities.*;
-//import io.javalin.Javalin;
-//import io.javalin.rendering.template.JavalinThymeleaf;
 import app.utils.Populate;
 import jakarta.persistence.EntityManagerFactory;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App {
     private static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-
-    public static void initiate() {
-        Populate populate = new Populate();
-        populate.populateDB();
+    private static final String API_KEY = System.getenv("GOOGLE_API_KEY");
 
 
-//        Javalin app = Javalin.create(config ->
-//        {
-//            config.staticFiles.add("/public");
-//            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-//            config.staticFiles.add("/templates");
-//        }).start(7072);
-        emf.close();
-    }
+    public static void initiate() throws IOException, InterruptedException {
+//
+//        double latitude = 55.68;
+//        double longitude = 12.57;
+//        int radius = 1000;
+//
+//        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+//                + "location=" + latitude + "," + longitude
+//                + "&radius=" + radius
+//                + "&keyword=playground"
+//                + "&key=" + API_KEY;
+//
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create(url))
+//                .GET()
+//                .build();
+//
+//        HttpResponse<String> response =
+//                client.send(request, HttpResponse.BodyHandlers.ofString());
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode root = mapper.readTree(response.body());
+//
+//        JsonNode results = root.get("results");
+//
+//        for (JsonNode place : results) {
+//
+//            String name = place.get("name").asText();
+//
+////            JsonNode location = place
+////                    .get("geometry")
+////                    .get("location");
+////
+////            double lat = location.get("lat").asDouble();
+////            double lng = location.get("lng").asDouble();
+//
+//            System.out.println("Name: " + name);
+//            System.out.println("Latitude: " + latitude);
+//            System.out.println("Longitude: " + longitude);
+//            System.out.println("----------------------------");
+//            Playground playground = Playground.builder()
+//                    .name(name)
+//                    .latitude(latitude)
+//                    .longitude(longitude)
+//                    .build();
+//            PlaygroundDAO playgroundDAO = new PlaygroundDAO(emf);
+//            playgroundDAO.create(playground);
+//        }
 
-    private void populateDB() {
-
+        Populate populate = new Populate(emf);
+        populate.populate();
     }
 }
 
