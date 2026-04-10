e71e76c1357bdd381f29de3185da033150bf92a2
luke-dkk <luke_persson@yahoo.dk>
2026-04-10 20:04:02 +0200

    Updated small mistake in Playground and PlaygroundDAO

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -32,7 +32,6 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @Builder.Default
     private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
